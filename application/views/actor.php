73a225eed7928f400639ac7480dac9194976b31c
Daniel <daniel51_1994@hotmail.com>
2017-09-14 19:33:27 -0700

    Add quotes to the individual pages
    Signed-off-by: Daniel <daniel51_1994@hotmail.com>

--- application/views/actor.php
+++ application/views/actor.php
@@ -3,5 +3,8 @@
         <a href="/welcome/show/{key}">
             <img src="/pix/{who}.jpg" title="{who} plays {role}"/>
         </a>
+        <h4>
+            {what}
+        </h4>
     </div>
 </div>
--- changelog.md
+++ changelog.md
@@ -4,6 +4,8 @@ Daniel
 Terence
 ## 0.0.1 - 2017-09-14
 ### Added
+
+- Quotes to individual pages
 - Hyperlinks to images that send to individual pages
 - Tooltip
 - Trump avatar
