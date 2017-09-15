6d998b88e422520e16f7dca145dbb7cb0d1e8ddf
Terence <viola_freak_123@hotmail.com>
2017-09-14 17:02:34 -0700

    Added Trump Avatar Signed-off-by:Terence <viola_freak_123@hotmail.com>

--- application/models/Quotes.php
+++ application/models/Quotes.php
@@ -24,7 +24,9 @@ class Quotes extends CI_Model
 		'5'	 => array('who'	 => 'Joan Rivers', 'role'	 => 'Dot Matrix',
 			'what'	 => 'Goodbye, virgin alarm.'),
 		'6'	 => array('who'	 => 'George Wyner', 'role'	 => 'Colonel Sandurz',
-			'what'	 => 'No, sir! I didn\'t see you playing with your dolls again!')
+			'what'	 => 'No, sir! I didn\'t see you playing with your dolls again!'),
+                '7'      => array('who'  => 'trump', 'role'       => 'Politician',
+                        'what'   => 'Sounds good, doesn\'t work')
 	);
 
 	// Constructor
